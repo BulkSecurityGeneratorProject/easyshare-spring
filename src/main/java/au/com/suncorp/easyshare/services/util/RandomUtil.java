b4856a061095dfa89ed768430743ef8bdadda6f5
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-09-18 00:30:16 -0400

    CVE-2019-16303 - JHipster Vulnerability Fix - Use CSPRNG in RandomUtil
    This fixes a security vulnerability in this project where the `RandomUtil.java`
    file(s) were using an insecure Pseudo Random Number Generator (PRNG) instead of
    a Cryptographically Secure Pseudo Random Number Generator (CSPRNG) for
    security sensitive data.
    
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- src/main/java/au/com/suncorp/easyshare/services/util/RandomUtil.java
+++ src/main/java/au/com/suncorp/easyshare/services/util/RandomUtil.java
@@ -2,13 +2,24 @@ package au.com.suncorp.easyshare.services.util;
 
 import org.apache.commons.lang.RandomStringUtils;
 
+import java.security.SecureRandom;
+
 /**
  * Utility class for generating random Strings.
  */
 
 public final class RandomUtil {
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
+    static {
+        SECURE_RANDOM.nextBytes(new byte[64]);
+    }
 
     private RandomUtil() {
+    }
+
+    private static String generateRandomAlphanumericString() {
+        return RandomStringUtils.random(DEF_COUNT, 0, 0, true, true, null, SECURE_RANDOM);
     }
 
     /**
@@ -17,6 +28,6 @@ public final class RandomUtil {
      * @return the generated password
      */
     public static String generateString(int count) {
-        return RandomStringUtils.randomAlphanumeric(count);
+        return generateRandomAlphanumericString();
     }
 }
